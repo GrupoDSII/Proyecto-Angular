c194f68ebfb308d71d1d7eb181b98629f9cf0b40
GrupoDSII <bryanalvaro556@gmail.com>
2022-05-06 00:14:55 -0700

    hi

---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styles: [
+  ]
+})
+export class ImgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
